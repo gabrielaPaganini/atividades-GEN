86231f789a1a353f75f18e18906ed248f32e7a61
gabriela paganini <gabrielapaganini67@gmail.com>
2021-06-11 19:00:24 -0300

    atividade java: Collections

---
+++ atividadeDia11-06/Colecao.java
@@ -0,0 +1,64 @@
+package atividade02;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class Colecao {
+
+		public static void main(String[] args) {
+			List<Integer>minhaLista = new ArrayList<Integer>();
+			
+			minhaLista.add(2);
+			minhaLista.add(1);
+			minhaLista.add(2);
+			minhaLista.add(3);
+			minhaLista.add(1);
+			
+			for(Integer lista:minhaLista) {
+				System.out.println(lista);
+			}
+			
+			System.out.println("\nRemovendo o elemento da minha lista...");
+			System.out.println();
+			minhaLista.remove(0);
+			
+			for(Integer lista:minhaLista) {
+				System.out.println(lista);
+			}
+			
+			int primeiroElemento = minhaLista.get(0);
+			System.out.println("\nO primeiro elemento é: "+primeiroElemento);
+			System.out.println();
+			
+			for(int i=0; i<minhaLista.size(); i++) {
+				System.out.println("\nElemento: "+minhaLista.get(i));
+			}
+			
+			Collections.sort(minhaLista);
+			System.out.println("\nDepois de ordenado...");
+			System.out.println(minhaLista);
+			
+			System.out.println();
+			
+			Set<Integer> meuSet = new HashSet<Integer>();
+			
+			meuSet.add(2);
+			meuSet.add(1);
+			meuSet.add(4);
+			meuSet.add(2);
+			meuSet.add(1);
+
+			Iterator<Integer> iMeuSet = meuSet.iterator();
+			
+			while(iMeuSet.hasNext()) {
+				System.out.println(iMeuSet.next());
+			}
+			
+
+	}
+
+}
---
+++ atividadeDia11-06/colecao2.java
@@ -0,0 +1,67 @@
+package atividade02;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class colecao2 {
+public static void main(String[] args) {
+	int op;
+	Scanner ler = new Scanner(System.in);
+	ArrayList<String> estoque = new ArrayList<String>();
+	
+	do {
+		System.out.println("\n\t\tDigite uma opção: ");
+		System.out.println("=================================");
+		System.out.println("\n(1) Deseja adicionar produtos ao estoque? \n(2) Deseja remover um produto?"
+				+ "\n(3) Atualizar produto \n(4) Mostrar todos os produtos do estoque \n(0) Encerrar o programa.");
+		op = ler.nextInt();
+		
+		switch(op)
+		{
+		case 1:
+			ler.nextLine();
+			System.out.println("\nDigite o nome do produto para adicionar ao estoque: ");
+			String produto = ler.nextLine();
+			estoque.add(produto);
+			break;
+		case 2:
+			ler.nextLine();
+			System.out.println("\nDigite o produto para remove-lo do estoque: ");
+			String produto1 = ler.nextLine();
+			if(estoque.contains(produto1)) {
+				estoque.remove(produto1);
+			}else {
+				System.out.println("\nProduto não existe!");
+			}
+			break;
+		case 3:{
+			ler.nextLine();
+			System.out.println("\nDigite o produto que deseja atualizar: ");
+			String verifica = ler.nextLine();
+			System.out.println("\nDigite o nome do produto que entrará no lucar de "+verifica+": ");
+			String novo = ler.nextLine();
+			if(estoque.contains(verifica)) {
+				estoque.remove(verifica);
+				estoque.add(novo);
+			}else {
+				System.out.println("\nProduto não existe no estoque!");
+			}
+			System.out.println(estoque);
+		}
+			break;
+		case 4:{
+				System.out.println("\nOs produtos do estoque são: ");
+				System.out.println(estoque);
+		}
+				break;
+				default:
+					System.out.println("\nFinalizou o programa!");
+		
+		}
+	
+	}
+	while(op!=0);
+	ler.close();
+
+}
+}
